470f07170d95a546857a657a87907af4b4ea2b6c
Ahmed-KHZ56 <eltwo3m@gmail.com>
2025-07-04 15:41:39 +0300

    Add backend project files to Backend folder

---
+++ Backend/HealLink/HealLink.Domain/Subscriptions/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealLink.Domain.Subscriptions
+{
+    public class Subscription
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+
+    }
+}
