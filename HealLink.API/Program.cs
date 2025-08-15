015c7357fce0f518a10f0d265618d10a7ab95e80
YoussefKamal <140238148+youssefkamal2030@users.noreply.github.com>
2025-08-15 14:58:50 +0300

    Update database connection to remote server
    Changed the `Main` method in `Program.cs` to use a new "RemoteConnection" string for the `HealLinkDbContext`, transitioning from a local to a remote database.
    
    Added a new connection string in `appsettings.json` for remote SQL Server access while retaining the existing local connection.
    
    Created a migration class `update_remote` in `20250815115453_update_remote.cs` with empty methods for future schema changes, along with its designer file to define the database schema for various entities in the `HealLink` domain.
    
    These changes facilitate the application's shift to a remote database setup.

--- HealLink.API/Program.cs
+++ HealLink.API/Program.cs
@@ -26,7 +26,7 @@ namespace HealLink
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<HealLinkDbContext>(options =>
-                 options.UseSqlServer(builder.Configuration.GetConnectionString("localConnection")));
+                 options.UseSqlServer(builder.Configuration.GetConnectionString("RemoteConnection")));
             builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<HealLinkDbContext>());
 
             // Add services to the container.  
