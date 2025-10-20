d84c6093fbc073c491200637534b5e3581eebe9c
YoussefKamal <140238148+youssefkamal2030@users.noreply.github.com>
2025-10-20 20:17:53 +0300

    Add mediator support and connection request event
    Updated `CreateConnectionRequestCommandHandler` to use `IMediator` for publishing `ConnectionRequestCreatedEvent`. Simplified error handling in the `Handle` method. Added new file `ConnectionRequestCreatedEvent.cs` to define the event structure.

--- healLink.Application/Handlers/Connection/CreateConnectionRequestCommandHandler.cs
+++ healLink.Application/Handlers/Connection/CreateConnectionRequestCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using healLink.Application.Commands.Connections;
 using healLink.Application.Common.Models;
 using healLink.Application.Repositories;
+using HealLink.Domain.DomainEvents;
 using HealLink.Domain.Entities;
 using MediatR;
 
@@ -10,26 +11,31 @@ namespace healLink.Application.Handlers.Connection
         : IRequestHandler<CreateConnectionRequestCommand, Result<CreateConnectionRequestResponse>>
     {
         private readonly IConnectionRequestsRepository _connectionRequestsRepository;
-
-        public CreateConnectionRequestCommandHandler(IConnectionRequestsRepository connectionRequestsRepository)
+        private readonly IMediator _mediator;
+        public CreateConnectionRequestCommandHandler(IConnectionRequestsRepository connectionRequestsRepository
+            ,IMediator mediator)
         {
             _connectionRequestsRepository = connectionRequestsRepository;
+            _mediator = mediator;
         }
 
-        public async Task<Result<CreateConnectionRequestResponse>> Handle(CreateConnectionRequestCommand request, CancellationToken cancellationToken)
+        public async Task<Result<CreateConnectionRequestResponse>> Handle(
+         CreateConnectionRequestCommand request,
+         CancellationToken cancellationToken)
         {
             try
             {
-                if(await _connectionRequestsRepository.ExistAsync(request.DoctorId,request.PatientId))
-                {
+                if (await _connectionRequestsRepository.ExistAsync(request.DoctorId, request.PatientId))
                     return Result<CreateConnectionRequestResponse>.Failure("Connection request already exists.");
-                }
+
                 var connectionRequest = new ConnectionRequest(request.DoctorId, request.PatientId);
                 var result = await _connectionRequestsRepository.AddConnectionAsync(connectionRequest);
+                await _mediator.Publish(new ConnectionRequestCreatedEvent(result.Id, result.DoctorId, result.PatientId), cancellationToken);
+
                 var response = new CreateConnectionRequestResponse(result.Id, result.Status);
                 return Result<CreateConnectionRequestResponse>.Success(response);
             }
-            catch (Exception ex)
+            catch
             {
                 return Result<CreateConnectionRequestResponse>.Failure("Failed to create connection request.");
             }
